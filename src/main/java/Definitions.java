c4f43561f0fb6750eda929d8ced1c4bc80199817
Yitzhak Amsalem <yitzhak@gmail.com>
2022-04-27 01:40:54 +0300

    Initial commit

---
+++ src/main/java/Definitions.java
@@ -0,0 +1,13 @@
+public class Definitions {
+    public static final int EMPTY = 0;
+    public static final int OBSTACLE = 1;
+    public static final int PAUSE_BEFORE_BACKTRACK = 0;
+    public static final int PAUSE_BEFORE_NEXT_SQUARE = 75;
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 600;
+
+    public static final int ALGORITHM_BRUTE_FORCE = 1;
+    public static final int ALGORITHM_DFS = 2;
+    public static final int ALGORITHM_BFS = 3;
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.util.Random;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String algorithmString = JOptionPane.showInputDialog("Choose algorithm (1 - brute force, 2 - DFS, 3 - BFS) ");
+        String sizeAsString = JOptionPane.showInputDialog("Choose maze size: ");
+        int size = Integer.parseInt(sizeAsString);
+        int algorithm = Integer.parseInt(algorithmString);
+        Maze maze = new Maze(algorithm, size, 0, 0);
+        maze.checkWayOut();
+    }
+
+}
